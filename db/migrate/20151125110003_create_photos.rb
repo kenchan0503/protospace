4b7f7933bba720ffb6c4f2502eda772c11383ee6
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-11-26 02:03:26 +0900

    [add]makePhotosTable

---
+++ db/migrate/20151125110003_create_photos.rb
@@ -0,0 +1,10 @@
+class CreatePhotos < ActiveRecord::Migration
+  def change
+    create_table :photos do |t|
+      t.integer :prototype_id
+      t.integer :status
+
+      t.timestamps null: false
+    end
+  end
+end
