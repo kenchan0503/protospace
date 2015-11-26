8110530a4959b55d4cc4747ff802f9c05b3fea3a
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-11-26 17:30:50 +0900

    [fix]idをinteger型からreference型に変更

--- db/migrate/20151125110003_create_photos.rb
+++ db/migrate/20151125110003_create_photos.rb
@@ -3,8 +3,11 @@ class CreatePhotos < ActiveRecord::Migration
     create_table :photos do |t|
       t.integer :prototype_id
       t.integer :status
+      t.references :prototype
 
       t.timestamps null: false
     end
+
+    add_index :photos, :prototype
   end
 end
