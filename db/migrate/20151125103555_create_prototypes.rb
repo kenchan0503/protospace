04379dee20ce24f0ce5c06dd6194d0d79cf17494
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-11-26 17:23:45 +0900

    [fix]idをinteger型からreferences型に変更

--- db/migrate/20151125103555_create_prototypes.rb
+++ db/migrate/20151125103555_create_prototypes.rb
@@ -5,9 +5,10 @@ class CreatePrototypes < ActiveRecord::Migration
       t.string :title
       t.text :catchcopy
       t.text :concept
-
+      t.references :user
 
       t.timestamps null: false
     end
+    add_index :prototypes, :user
   end
 end
