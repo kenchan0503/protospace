6b87590c3aacb2ac6009a342c44249d139937e31
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-11-27 18:46:20 +0900

    [fix]

--- db/migrate/20151126132948_create_likes.rb
+++ db/migrate/20151126132948_create_likes.rb
@@ -1,7 +1,7 @@
 class CreateLikes < ActiveRecord::Migration
   def change
     create_table :likes do |t|
-      t.integer :like_count, default => 0
+      t.integer :like_count, :default => 0
       t.references :prototype
 
       t.timestamps null: false
