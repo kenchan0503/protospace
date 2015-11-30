4b6e8c8fea4df67c7851218d708800fb137a641d
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-11-30 23:41:56 +0900

    [fix]change_controllr_name

--- app/controllers/protos_controller.rb
+++
@@ -1,2 +0,0 @@
-class ProtosController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'top#index'
   resources :top, only: [:index]
   resources :user, only: [:show, :new, :create, :edit, :update]
-  resources :proto, only: [:show, :new, :create]
+  resources :prototypes, only: [:show, :new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
