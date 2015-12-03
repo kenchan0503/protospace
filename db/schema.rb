c03e8fbd4283e7f5609eb594d8398d67c31ee7df
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-03 21:16:21 +0900

    [add]add_column_user_name_to_usersmodel

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201082440) do
+ActiveRecord::Schema.define(version: 20151203121236) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text",         limit: 65535
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20151201082440) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
+    t.string   "user_name",              limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
