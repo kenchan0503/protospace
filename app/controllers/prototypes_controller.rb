c26384489107a7fbe12c1252e25edc3e02c4d8fe
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-05 18:09:06 +0900

    [def]define_create_method_in_prototypes_controller

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,6 +3,9 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.new
   end
 
+  def create
+  end
+
   def prototype_params
     params.require(:prototype).permit(:title, :catchcopy, :concept, :avatar)
   end
