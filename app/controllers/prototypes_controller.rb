3b13229b845a72a16ee8a32e4860aef51ce8db62
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-05 11:24:20 +0900

    [add]prototypeのnewアクション、ストロングパラメーターのメソッドを追加

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,2 +1,8 @@
 class PrototypesController < ApplicationController
+  def new
+  end
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catchcopy, :concept, :avatar)
+  end
 end
