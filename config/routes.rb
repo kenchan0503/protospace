2ccb0d1b43bbc85aa79aefc3b2075b5d393331ac
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-10 18:27:12 +0900

    [add]prototypeのルーティングにeditとupdateを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'top#index'
   resources :top, only: [:index]
   resources :user, only: [:show, :new, :create, :edit, :update, :destroy]
-  resources :prototypes, only: [:show, :new, :create]
+  resources :prototypes, only: [:show, :new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
