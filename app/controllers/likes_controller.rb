d82903ce086ccae627b9e40b5c3c43d6be48098b
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-17 18:33:25 +0900

    [def]ajaxを定義。redirect_to actionを削除。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,13 +3,11 @@ class LikesController < ApplicationController
     @prototype = Prototype.find(params[:prototype])
     @like = current_user.likes.build(prototype_id: @prototype.id)
     @like.save
-    redirect_to @prototype
   end
 
   def unlike
     @prototype = Prototype.find(params[:prototype])
     @like = current_user.likes.find_by(prototype_id: @prototype.id)
     @like.destroy
-    redirect_to @prototype
   end
 end
