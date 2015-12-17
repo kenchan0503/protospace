9af8eca1f1f2f899ffa3133b38c30f0ab38df090
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-17 17:38:23 +0900

    [def]like actionとunlike actionを定義
    まだajaxを導入していないので現段階ではredirect_toを使用している

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,15 @@
 class LikesController < ApplicationController
   def like
+    @prototype = Prototype.find(params[:prototype])
+    @like = current_user.likes.build(prototype_id: @prototype.id)
+    @like.save
+    redirect_to @prototype
   end
 
   def unlike
+    @prototype = Prototype.find(params[:prototype])
+    @like = current_user.likes.build(prototype_id: @prototype.id)
+    @like.destroy
+    redirect_to @protoytpe
   end
 end
