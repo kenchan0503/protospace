1c7c0785f2b72858d14c7b2deab0da095588baa6
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-18 18:27:14 +0900

    [def]indexページのメソッド名を変更
    最新順はnew_orderメソッド、人気順はlike_orderメソッドで管理する。

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,8 @@
 class TopController < ApplicationController
-  def index
+  def new_order
     @prototypes = Prototype.all.order("created_at DESC").page(params[:page]).per(5)
+  end
+
+  def like_order
   end
 end
