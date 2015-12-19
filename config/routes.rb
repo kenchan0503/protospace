b677b37f5585b71f90bb3d8802e0e2f0017c0ff3
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-19 16:44:20 +0900

    [fix]like,unlike actionをcreate,destroy actionに変えたので、ルーティングもそれに対応して修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :top, only: [:index]
   resources :user, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :prototypes, only: [:show, :new, :create]
-  post 'like/:prototype.id' => 'likes#like', as: 'like'
-  delete 'unlike/:prototype.id' => 'likes#unlike', as: 'unlike'
+  post 'like/:prototype.id' => 'likes#create', as: 'like'
+  delete 'unlike/:prototype.id' => 'likes#destroy', as: 'unlike'
   resources :prototypes, only: [:show, :new, :create, :edit, :update, :destroy]
 end
