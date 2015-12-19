fc5abffd086c6e908b58eb63990474d69475f2c3
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-20 08:34:12 +0900

    [fix]top_controllerのactionを一つにまとめた。
    新しい順、いいね順の二つのインスタンスをあらかじめ取得し、一方をビューで表示させるようにする。

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,9 +1,6 @@
 class TopController < ApplicationController
-  def new_order
-    @prototypes = Prototype.all.order("created_at DESC").page(params[:page]).per(5)
-  end
-
-  def like_order
+  def index
+    @new_order_prototypes = Prototype.all.order("created_at DESC").page(params[:page]).per(5)
     prototype_ids = Like.group(:prototype_id).order('count_prototype_id DESC').count(:prototype_id).keys
     @prototypes = prototype_id.map{ |id| Prototype.find(id) }
   end
