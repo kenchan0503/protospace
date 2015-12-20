5edbdfe31e501b75cbde808ac9115210ff16de16
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-20 13:53:39 +0900

    [fix]pageメソッドはActiverecordRelationでしか使えないのでpageメソッドの位置を修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,8 @@
 class TopController < ApplicationController
   def index
-    @new_order_prototypes = Prototype.all.order("created_at DESC").page(params[:page]).per(5)
-    prototype_ids = Like.group(:prototype_id).order('count_prototype_id DESC').count(:prototype_id).keys
-    @prototypes = prototype_id.map{ |id| Prototype.find(id) }
+    binding.pry
+    @new_order_prototypes = Prototype.order("created_at DESC").page(params[:page]).per(5)
+    prototype_ids = Like.group(:prototype_id).order('count_prototype_id DESC').page(params[:page]).per(5).count(:prototype_id).keys
+    @like_order_prototypes = prototype_ids.map{ |id| Prototype.find(id) }
   end
 end
