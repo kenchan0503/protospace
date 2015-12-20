38a60f21a47774b22272a5bb7f0fb5baee358672
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-20 16:01:12 +0900

    [fix]不要な行を削除

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,11 @@ class LikesController < ApplicationController
   before_action :set_prototype
 
   def create
-    @prototype = Prototype.find(params[:prototype])
     @like = current_user.likes.build(prototype_id: @prototype.id)
     @like.save
   end
 
   def destroy
-    @prototype = Prototype.find(params[:prototype])
     @like = current_user.likes.find_by(prototype_id: @prototype.id)
     @like.destroy
   end
