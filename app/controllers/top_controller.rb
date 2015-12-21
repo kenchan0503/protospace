ce4a08bd3dc1016b40eb03aca2ffa83f5d3cb8c6
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-21 21:39:04 +0900

    [del]top_controllerを削除
    top_controllerのindexアクションはprototypes_controllerのindexが担う

--- app/controllers/top_controller.rb
+++
@@ -1,8 +0,0 @@
-class TopController < ApplicationController
-  def index
-    binding.pry
-    @new_order_prototypes = Prototype.order("created_at DESC").page(params[:page]).per(5)
-    prototype_ids = Like.group(:prototype_id).order('count_prototype_id DESC').page(params[:page]).per(5).count(:prototype_id).keys
-    @like_order_prototypes = prototype_ids.map{ |id| Prototype.find(id) }
-  end
-end
