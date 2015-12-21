2eacfbf4166aecbbb84ba09594f1faaacb534d7a
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-21 18:24:03 +0900

    [fix]likes_controllerのルーティングをresourcesメソッドを使って定義

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root 'prototypes#index'
   resources :top, only: [:index]
   resources :user, only: [:show, :new, :create, :edit, :update, :destroy]
-  resources :prototypes
-  post 'like/:prototype.id' => 'likes#create', as: 'like'
-  delete 'unlike/:prototype.id' => 'likes#destroy', as: 'unlike'
+  resources :prototypes do
+    resources :likes, only: [:create, :destroy]
+  end
   resources :prototypes, only: [:show, :new, :create, :edit, :update, :destroy]
 end
