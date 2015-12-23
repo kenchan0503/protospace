f7aeb9363df9e67b43860a3ddaf93f44bf70f41b
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-23 10:26:03 +0900

    [def]create,destroy時にそれぞれlike_countが１増減するように定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,11 +3,14 @@ class LikesController < ApplicationController
 
   def create
     @like = current_user.likes.build(prototype_id: @prototype.id)
+    @like.prototype.update(like_count: @like.prototype.like_count + 1)
     @like.save
   end
 
   def destroy
+    @like = current_user.likes.build(prototype_id: @prototype.id)
     Like.find(params[:id]).destroy
+    @like.prototype.update(like_count: @like.prototype.like_count - 1)
   end
 
   private
