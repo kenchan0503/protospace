c15a3753204b64ba038bf1d9d1b613e3d2777e34
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-24 08:07:52 +0900

    [def]入力されたデータを保存。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
 class CommentsController < ApplicationController
   before_action :set_prototype
   def create
+    current_user.comments.create(prototype_id: @prototype.id, text: params[:comment][:text])
+    redirect_to :@prototype
   end
 
   private
   def set_prototype
-    @protoytpe = protoytpe.find(params[:id])
+    @prototype = Prototype.find(params[:prototype_id])
   end
 end
