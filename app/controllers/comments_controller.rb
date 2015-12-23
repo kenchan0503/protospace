35249efde2ddb45858af9d6c4cda68330c671b4e
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-23 21:25:48 +0900

    [def]set_prototype_as_before_action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+  before_action :set_prototype
+  def create
+  end
+
+  private
+  def set_prototype
+    @protoytpe = protoytpe.find(params[:id])
+  end
 end
