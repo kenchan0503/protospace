086d0c8321da9df4c921148fe14c43a9ce801326
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-24 18:30:06 +0900

    [def]prototype modelにacts_as_taggble_on メソッドを追加

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,6 +3,7 @@ class Prototype < ActiveRecord::Base
   has_many :comments
   has_many :photos
   has_many :likes, dependent: :destroy
+  acts_as_taggable_on :prototypes
 
   accepts_nested_attributes_for :photos
   validates :title, :catchcopy, :concept, presence: true
