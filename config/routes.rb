22c7dee378d75e5fb2bd360d23398455acd4d47a
kenchan0503 <k.taninaka1994+1@gmail.com>
2015-12-26 12:15:21 +0900

    [def]ルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
     resources :likes, only: [:create, :destroy]
   end
   resources :prototypes, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :tags, only: [:index, :show]
 end
