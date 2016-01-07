2f8fc415f4e51a0c588008b4ffbef636c3696b17
kenchan0503 <k.taninaka1994+1@gmail.com>
2016-01-07 17:23:05 +0900

    [add]全てのフォームでjsを適用

--- app/assets/javascripts/prototype.js
+++ app/assets/javascripts/prototype.js
@@ -21,4 +21,46 @@ $(function() {
     }
     reader.readAsDataURL(file);
   });
+
+  $("#prototype_photo_url_sub_0").change(function() {
+    var file = $(this).prop('files')[0];
+
+    // 画像以外は処理を停止
+    if (! file.type.match('image.*')) {
+      // クリア
+      $(this).val('');
+      $('#prototype_photo_url_main_0').html('');
+      return;
+    }
+
+    // 画像表示
+    var reader = new FileReader();
+    reader.onload = function() {
+      var main_src = $("#sub_src_0").attr('src', reader.result);
+      $('#sub_src_0').html(main_src);
+    }
+    reader.readAsDataURL(file);
+  });
+
+  $("#prototype_photo_url_main_1").change(function() {
+    var file = $(this).prop('files')[0];
+
+    // 画像以外は処理を停止
+    if (! file.type.match('image.*')) {
+      // クリア
+      $(this).val('');
+      $('#prototype_photo_url_main_1').html('');
+      return;
+    }
+
+    // 画像表示
+    var reader = new FileReader();
+    reader.onload = function() {
+      var main_src = $("#sub_src_1").attr('src', reader.result);
+      $('#main_src').html(main_src);
+    }
+    reader.readAsDataURL(file);
+  });
 });
+
+
