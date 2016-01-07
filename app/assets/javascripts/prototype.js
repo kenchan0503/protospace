bb3bfc90c9704c282aa5ca42330578b5657152b7
kenchan0503 <k.taninaka1994+1@gmail.com>
2016-01-07 21:31:53 +0900

    [fix]コードをdryした

--- app/assets/javascripts/prototype.js
+++ app/assets/javascripts/prototype.js
@@ -1,65 +1,27 @@
 $(function() {
-
-  // アップロードするファイルを選択
-  $("#prototype_photo_url_main").change(function() {
-    var file = $(this).prop('files')[0];
-
-    // 画像以外は処理を停止
-    if (! file.type.match('image.*')) {
-      // クリア
-      $(this).val('');
-      $('#prototype_photo_url_main').html('');
-      return;
-    }
-
-    // 画像表示
-    var reader = new FileReader();
-    reader.onload = function() {
-      var main_src = $("#main_src").attr('src', reader.result);
-      $('#main_src').html(main_src);
-    }
-    reader.readAsDataURL(file);
-  });
-
-  $("#prototype_photo_url_sub_0").change(function() {
-    var file = $(this).prop('files')[0];
-
-    // 画像以外は処理を停止
-    if (! file.type.match('image.*')) {
-      // クリア
-      $(this).val('');
-      $('#prototype_photo_url_main_0').html('');
-      return;
-    }
-
-    // 画像表示
-    var reader = new FileReader();
-    reader.onload = function() {
-      var main_src = $("#sub_src_0").attr('src', reader.result);
-      $('#sub_src_0').html(main_src);
-    }
-    reader.readAsDataURL(file);
-  });
-
-  $("#prototype_photo_url_main_1").change(function() {
-    var file = $(this).prop('files')[0];
-
-    // 画像以外は処理を停止
-    if (! file.type.match('image.*')) {
-      // クリア
-      $(this).val('');
-      $('#prototype_photo_url_main_1').html('');
-      return;
-    }
-
-    // 画像表示
-    var reader = new FileReader();
-    reader.onload = function() {
-      var main_src = $("#sub_src_1").attr('src', reader.result);
-      $('#main_src').html(main_src);
-    }
-    reader.readAsDataURL(file);
-  });
+  add_image("#prototype_photo_url_main", "#main_src");
+  add_image("#prototype_photo_url_sub_0", "#sub_src_0")
+  add_image("#prototype_photo_url_sub_1", "#sub_src_1")
 });
+  // アップロードするファイルを選択
+  function add_image(photo_url, src){
+    $(photo_url).change(function() {
+      var file = $(this).prop('files')[0];
 
+      // 画像以外は処理を停止
+      if (! file.type.match('image.*')) {
+        // クリア
+        $(this).val('');
+        $(photo_url).html('');
+        return;
+      }
 
+      // 画像表示
+      var reader = new FileReader();
+      reader.onload = function() {
+        var main_src = $(src).attr('src', reader.result);
+        $(src).html(main_src);
+      }
+      reader.readAsDataURL(file);
+    });
+  };
