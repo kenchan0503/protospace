0a1596b107de3d4938d7d81a8fe56fefa72a2704
kenchan0503 <k.taninaka1994+1@gmail.com>
2016-01-08 22:44:36 +0900

    [def]update_paramsを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,15 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user.update(update_params)
   end
 
   private
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def update_params
+    params.require(:user).permit(:use_name, :email, :group_name, :works, :profile, :avatar)
+  end
 end
