2f0886dd8b47698d5384229068882713bca3a28a
kenchan0503 <k.taninaka1994+1@gmail.com>
2016-01-15 04:33:07 +0900

    [fix]develpment.ymlに定義した変数を使うために記法を修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: AWS_ACCESS_KEY_ID,
-    aws_secret_access_key: AWS_SECRET_ACCESS_KEY ,
+    aws_access_key_id: Settings[:AWS_ACCESS_KEY_ID],
+    aws_secret_access_key: Settings[:AWS_SECRET_ACCESS_KEY] ,
     region: 'ap-northeast-1'
   }
 
